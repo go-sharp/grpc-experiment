8b7133ce8376f871919eb87f78bf463e733ee3ac
Sandro Dallo <sandro@dallo.biz>
2018-03-31 01:11:04 +0200

    Proxy with Swagger UI finished

--- src/grpc-proxy/main.go
+++ src/grpc-proxy/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
-	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -28,14 +30,42 @@ func run() error {
 		return err
 	}
 
-	return http.ListenAndServe(":8080", mux)
+	dispatcher := http.NewServeMux()
+	dispatcher.Handle("/v1/", mux)
+	dispatcher.Handle("/swagger/", http.StripPrefix("/swagger", http.FileServer(http.Dir("./swagger"))))
+	dispatcher.HandleFunc("/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		content, err := ioutil.ReadFile("./todo.swagger.json")
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+		w.Write(content)
+	})
+	// swaggerUI := http.FileServer(http.Dir("./swagger"))
+
+	// handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.ProtoMajor == 2 && r.Header.Get("Content-Type") == "application/grpc" {
+	// 		mux.ServeHTTP(w, r)
+	// 	} else if r.URL.Path == "/swagger.json" {
+	// 		content, err := ioutil.ReadFile("./todo.swagger.json")
+	// 		if err != nil {
+	// 			fmt.Fprintf(w, "%v", err)
+	// 			return
+	// 		}
+	// 		w.Write(content)
+	// 	} else {
+	// 		swaggerUI.ServeHTTP(w, r)
+	// 	}
+	// })
+
+	log.Printf("starting grpc proxy on port :8080...")
+	return http.ListenAndServe(":8080", dispatcher)
 }
 
 func main() {
 	flag.Parse()
-	defer glog.Flush()
 
 	if err := run(); err != nil {
-		glog.Fatal(err)
+		log.Fatal(err)
 	}
 }
