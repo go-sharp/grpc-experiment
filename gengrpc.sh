aa55395aa6560176c08a23b5a18ac08e1af0e80c
Sandro Dallo <sandro@dallo.biz>
2018-04-07 23:01:27 +0200

    Proxy generation script added and binaries

--- gengrpc.sh
+++ gengrpc.sh
@@ -3,7 +3,7 @@
 echo "Generating C# assets"
 $PROTOC_PATH/protoc --csharp_out=./src/grpc-service --grpc_out=./src/grpc-service  --plugin=protoc-gen-grpc=$PROTOC_PATH/grpc_csharp_plugin ./src/protobuf/todo.proto
 echo "Generating GRPC Proxy"
-$PROTOC_PATH/protoc -I ./src/protobuf  --go_out=plugins=grpc:./src/grpc-proxy/grpcproxy ./src/protobuf/proxy.proto
-$PROTOC_PATH/protoc -I ./src/protobuf  --grpc-gateway_out=logtostderr=true:./src/grpc-proxy/grpcproxy ./src/protobuf/proxy.proto
+$PROTOC_PATH/protoc -I ./src/protobuf  --go_out=plugins=grpc:./src/grpc-proxy/grpcproxy --plugin=protoc-gen-go=$PROTOC_PATH/protoc-gen-go ./src/protobuf/proxy.proto
+$PROTOC_PATH/protoc -I ./src/protobuf  --grpc-gateway_out=logtostderr=true:./src/grpc-proxy/grpcproxy --plugin=protoc-gen-grpc-gateway=$PROTOC_PATH/protoc-gen-grpc-gateway ./src/protobuf/proxy.proto
 echo "Generating GRPC Proxy Swagger definiton"
-$PROTOC_PATH/protoc -I ./src/protobuf  --swagger_out=logtostderr=true:./src/grpc-proxy ./src/protobuf/proxy.proto
+$PROTOC_PATH/protoc -I ./src/protobuf  --swagger_out=logtostderr=true:./src/grpc-proxy --plugin=protoc-gen-swagger=$PROTOC_PATH/protoc-gen-swagger ./src/protobuf/proxy.proto
