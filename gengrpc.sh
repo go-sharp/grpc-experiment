b1a277d4f92c44b25dca2fa1a2c20dc919c2a48e
Sandro Dallo <sandro@dallo.biz>
2018-04-07 23:21:10 +0200

    Typescript generation added to script

--- gengrpc.sh
+++ gengrpc.sh
@@ -7,3 +7,8 @@ $PROTOC_PATH/protoc -I ./src/protobuf  --go_out=plugins=grpc:./src/grpc-proxy/gr
 $PROTOC_PATH/protoc -I ./src/protobuf  --grpc-gateway_out=logtostderr=true:./src/grpc-proxy/grpcproxy --plugin=protoc-gen-grpc-gateway=$PROTOC_PATH/protoc-gen-grpc-gateway ./src/protobuf/proxy.proto
 echo "Generating GRPC Proxy Swagger definiton"
 $PROTOC_PATH/protoc -I ./src/protobuf  --swagger_out=logtostderr=true:./src/grpc-proxy --plugin=protoc-gen-swagger=$PROTOC_PATH/protoc-gen-swagger ./src/protobuf/proxy.proto
+echo "Generating Typescript-fetch client"
+java -jar ./tools/swagger-codegen-cli-2.2.1.jar  generate -i ./src/grpc-proxy/proxy.swagger.json -l typescript-fetch -o temp
+echo "/* tslint:disable */" > ./src/webapp/src/api/api.ts
+cat ./temp/api.ts >> ./src/webapp/src/api/api.ts
+rm -rf ./temp
